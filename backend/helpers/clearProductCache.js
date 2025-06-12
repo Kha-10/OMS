28c64482c4681f9bb23e9adcce5f9e7b048af1ca
Aung <august810.a@gmail.com>
2025-06-12 18:46:04 +0700

    removed old clearproductCache func

--- backend/helpers/clearProductCache.js
+++ backend/helpers/clearProductCache.js
@@ -1,22 +1,5 @@
 const redis = require("../config/redisClient");
 
-// async function clearProductCache() {
-//   try {
-//     // Find all keys matching the product cache pattern
-//     const keys = await redis.keys("products:*");
-
-//     if (keys.length > 0) {
-//       console.log('kk',keys);
-//       await redis.del(...keys);
-//       console.log(`Cleared ${keys.length} product cache keys.`);
-//     } else {
-//       console.log("No product cache keys found.");
-//     }
-//   } catch (err) {
-//     console.error("Error clearing product cache:", err);
-//   }
-// }
-
 const clearProductCache = async () => {
   const keys = await redis.keys("products:page*");
   if (keys.length > 0) {
