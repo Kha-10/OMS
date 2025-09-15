65fb81555da18ab8b4533bba72e0a4d079994ba7
Aung <august810.a@gmail.com>
2025-09-15 21:04:51 +0700

    added error message

--- backend/middlewares/checkMemberMiddleware.js
+++ backend/middlewares/checkMemberMiddleware.js
@@ -3,7 +3,11 @@ const StoreMember = require("../models/StoreMember");
 const checkMemberMiddleware = async (req, res, next) => {
   const userId = req.user.id;
   const storeId = req.params.storeId;
-
+  if (!userId || !storeId) {
+    return res
+      .status(403)
+      .json({ message: "Access denied: Missing user or store information." });
+  }
   const member = await StoreMember.findOne({ store: storeId, user: userId });
   if (!member) return res.status(403).json({ message: "Not a store member" });
 
